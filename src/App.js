16a90d5dc433a338bd7c292167464bc6a560fa57
naser <naser.muhana0@gmail.com>
2022-03-31 16:01:02 +0300

    change to lazy loading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Comments from "./components/comments/Comments";
-import LoadComments from "./components/comments/LoadComments";
 import Layout from "./components/layout/Layout";
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import QuoteDetail from "./pages/QuoteDetail";
+// import AllQuotes from "./pages/AllQuotes";
+// import NewQuote from "./pages/NewQuote";
+// import NotFound from "./pages/NotFound";
+// import QuoteDetail from "./pages/QuoteDetail";
+import LoadComments from "./components/comments/LoadComments";
+import Comments from "./components/comments/Comments";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
 
 const App = () => {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate to={"/quotes"} />} />
-        <Route path="/quotes" element={<AllQuotes />} />
-        <Route path="/quotes/:quoteId" element={<QuoteDetail />}>
-          <Route path={""} element={<LoadComments />} />
-          <Route path="comments" element={<Comments />} />
-        </Route>
-        <Route path="/new-quote" element={<NewQuote />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to={"/quotes"} />} />
+          <Route path="/quotes" element={<AllQuotes />} />
+          <Route path="/quotes/:quoteId" element={<QuoteDetail />}>
+            <Route path={""} element={<LoadComments />} />
+            <Route path="comments" element={<Comments />} />
+          </Route>
+          <Route path="/new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 };
